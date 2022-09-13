0feae6ecac8d8bbbb37be4c443f4ba06df2eab3a
Piyush <thesourcepedia@gmail.com>
2022-09-13 11:17:26 +0530

    feat(auth): changes in UI and config of auth

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,54 @@
 <template>
-  <router-view></router-view>
+  <authenticator :form-fields="formFields">
+    <template v-slot="{ user, signOut }">
+      <template v-if="user">
+        <router-view :user="user"></router-view>
+        <h1>Hello {{ user.username }}!</h1>
+        <button @click="signOut">Sign Out</button>
+      </template>
+    </template>
+  </authenticator>
 </template>
 
-<script>
-export default {
-  name: "App",
-  components: {},
+<script setup>
+import "@aws-amplify/ui-vue/styles.css";
+import { Amplify, Auth } from "aws-amplify";
+import awsconfig from "./aws-exports";
+
+Amplify.configure(awsconfig);
+Auth.configure(awsconfig);
+
+const formFields = {
+  signIn: {
+    username: {
+      labelHidden: true,
+      placeholder: "Enter your username",
+      isRequired: true,
+      label: "",
+    },
+  },
+  signUp: {
+    username: {
+      labelHidden: true,
+      placeholder: "Create username",
+      isRequired: true,
+      label: "",
+    },
+    email: {
+      labelHidden: true,
+      placeholder: "Enter Your Email Here",
+      isRequired: true,
+      label: "",
+    },
+  },
+  resetPassword: {
+    username: {
+      labelHidden: true,
+      placeholder: "Enter your username",
+      isRequired: true,
+      label: "",
+    },
+  },
 };
 </script>
 
@@ -17,5 +60,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.amplify-button[data-variation="link"]:hover {
+  background: none;
+  text-decoration: underline;
 }
 </style>
