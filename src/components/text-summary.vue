3357b0a9c318e94d2fa8c45c884afc3ebb707e16
Piyush <thesourcepedia@gmail.com>
2022-09-23 12:14:45 +0530

    feat: add sentence length input, ui changes

--- src/components/text-summary.vue
+++ src/components/text-summary.vue
@@ -6,7 +6,7 @@
           <label
             for="message"
             class="block mb-2 text-sm font-medium text-tertary"
-            >Your message</label
+            >Input Document</label
           >
           <textarea
             id="message"
@@ -17,21 +17,27 @@
           ></textarea>
         </div>
       </div>
-      <div class="flex mx-3 w-100">
-        <button
-          @click="getSummary"
-          type="button"
-          class="hover:animate-bounce inline-block px-5 py-2 bg-tertary text-primary font-medium text-md leading-wide rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg"
-        >
-          GENERATE
-        </button>
+      <div class="mx-3 w-100">
+        <div class="w-3/2">
+          <label for="sentenceLength" class="text-tertary mb-2"
+            >Sentence Length</label
+          >
+          <input placeholder="Length" v-model="sentenceLength" id="sentenceLength"  style="width: 100px"/>
+          <button
+            @click="getSummary"
+            type="button"
+            class="hover:animate-bounce inline-block px-5 py-2 mt-3 bg-tertary text-primary font-medium text-md leading-wide rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg"
+          >
+            GENERATE
+          </button>
+        </div>
       </div>
       <div class="flex-shrink w-full h-full">
         <label
           for="message"
           class="block mb-2 text-sm font-medium text-tertary"
         >
-          Your message
+          Document Summary
         </label>
         <p
           id="summary-section"
@@ -48,11 +54,11 @@ import { API } from "aws-amplify";
 let doc = ref(
   "Computer programming is the process of performing a particular computation (or more generally, accomplishing a specific computing result), usually by designing and building an executable computer program. Programming involves tasks such as analysis, generating algorithms, profiling algorithms' accuracy and resource consumption, and the implementation of algorithms (usually in a chosen programming language, commonly referred to as coding). The source code of a program is written in one or more languages that are intelligible to programmers, rather than machine code, which is directly executed by the central processing unit. The purpose of programming is to find a sequence of instructions that will automate the performance of a task (which can be as complex as an operating system) on a computer, often for solving a given problem. Proficient programming thus usually requires expertise in several different subjects, including knowledge of the application domain, specialized algorithms, and formal logic. Tasks accompanying and related to programming include testing, debugging, source code maintenance, implementation of build systems, and management of derived artifacts, such as the machine code of computer programs. These might be considered part of the programming process, but often the term software development is used for this larger process with the term programming, implementation, or coding reserved for the actual writing of code. Software engineering combines engineering techniques with software development practices. Reverse engineering is a related process used by designers, analysts, and programmers to understand and re-create/re-implement."
 );
-
+let sentenceLength = ref(35)
 async function getSummary() {
   const summary = await API.post("expressapi", "/nlp", {
     body: {
-      sentLength: 35,
+      sentLength: sentenceLength.value,
       doc: doc.value,
     },
   });
